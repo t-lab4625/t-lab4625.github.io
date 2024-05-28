0c57f8a9cdc8f74030a63f0f11df6c1a2b0a16c7
T <130152394+t-lab4625@users.noreply.github.com>
2024-05-28 16:34:18 +0900

    Create calcScore.js

---
+++ calcScore.js
@@ -0,0 +1,75 @@
+// ページが読み込まれた時の処理
+window.addEventListener("load", () => { 
+	// 値の読み込み
+	var jsonVocal = localStorage.getItem('vocal'),
+		jsonDance = localStorage.getItem('dance'),
+		jsonVisual = localStorage.getItem('visual');
+	// 書き込み
+	document.getElementById('vocal').value = JSON.parse(jsonVocal);
+	document.getElementById('dance').value = JSON.parse(jsonDance);
+	document.getElementById('visual').value = JSON.parse(jsonVisual);
+
+	calc();
+});
+
+// 計算・表示
+var calc = function () {
+	// 各評価ランクのボーダー
+	var s = 13000,
+		a_plus = 11500,
+		a = 10000,
+		b_plus = 8000,
+		b = 6000 - 1700,
+		c_plus = 4500,
+		c = 3000,
+		// 評価ランクスコア
+		rank = [s, a_plus, a, b_plus, b, c_plus, c],
+		// 評価ランク名
+		rankName = ['s', 'a_plus', 'a', 'b_plus', 'b', 'c_plus', 'c'],
+		// 各ステータス値
+		vocal = document.getElementById('vocal').value,
+		dance = document.getElementById('dance').value,
+		visual = document.getElementById('visual').value;
+
+	// 現在の評価点(ステータスと順位,最終試験後の+30込み)
+	var nowScore = parseInt(((
+		Math.min(parseInt(vocal) + 30, 1500)
+		+ Math.min(parseInt(dance) + 30, 1500)
+		+ Math.min(parseInt(visual) + 30, 1500)) * 23) / 10) + 1700;
+
+	// ボーダーから現在の評価点を減算
+	for (var i = 0; i < rank.length; i++) {
+		rank[i] -= nowScore;
+	}
+
+	// 出力
+	for (var i = 0; i < rank.length && i < rankName.length; i++) {
+		document.getElementById('score_' + rankName[i]).innerHTML = Math.max(calcFinalExamScore(rank[i]), 0);
+	}
+
+	// 値を保存
+	jsonVocal = JSON.stringify(vocal);
+	jsonDance = JSON.stringify(dance);
+	jsonVisual = JSON.stringify(visual);
+	localStorage.setItem('vocal', jsonVocal);
+	localStorage.setItem('dance', jsonDance);
+	localStorage.setItem('visual', jsonVisual);
+}
+
+// 評価点を渡すと、その評価点を得るために必要な最終試験のスコアの最低値を返す
+var calcFinalExamScore = function (needScore) {
+	if (needScore <= 0) return 0;
+
+	if (needScore <= 1500)
+		return Math.ceil(needScore / 0.3);
+	else if (needScore <= 2250)
+		return 5000 + Math.ceil((needScore - 1500) / 0.15);
+	else if (needScore <= 3050)
+		return 10000 + Math.ceil((needScore - 2250) / 0.08);
+	else if (needScore <= 3450)
+		return 20000 + Math.ceil((needScore - 3050) / 0.04);
+	else if (needScore <= 3650)
+		return 30000 + Math.ceil((needScore = 3450) / 0.02);
+	else
+		return 40000 + Math.ceil((needScore - 3650) / 0.01);
+}
